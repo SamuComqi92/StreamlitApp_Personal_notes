9713ae203fb5d5c2b5e28ca7fd5e6376a3dd1d44
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-08-27 15:42:04 +0200

    Update App_personal_notes.py

--- App_personal_notes.py
+++ App_personal_notes.py
@@ -18,7 +18,7 @@ def load_text(filename):
 Menu_icon = "menu-up"
 Main_topics = ["Landing page", "Microsoft Fabric", 'Qlik (work in progress)']
 Main_icons = ['house', 'clipboard-data', 'graph-up-arrow']
-Section_Fabric = ["""📃 1. Fabric OneLake""", "🏟 2. Data Engineering - Lakehouse"]
+Section_Fabric = ["""1. Fabric OneLake""", "🏟 2. Data Engineering - Lakehouse"]
 Section_Fabric_icons = ['caret-right-fill', 'caret-right-fill']
 Section_Qlik = [""]
 Section_Qlik_icons = ['caret-right-fill', 'caret-right-fill']
