882206e25f88f7957d9aaca73342dbcbd6236f57
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-08-27 09:59:48 +0200

    Update App_personal_notes.py

--- App_personal_notes.py
+++ App_personal_notes.py
@@ -10,7 +10,7 @@ with st.sidebar:
     selected = option_menu(
         "Topics", 
         ["Microsoft Fabric", 'Qlik'], 
-        icons = ['house', 'gear'], 
+        icons = ['✅house', 'gear'], 
         menu_icon = "cast", 
         default_index = 0,
         styles = {
