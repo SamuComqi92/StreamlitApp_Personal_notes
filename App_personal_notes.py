757686d9b2046fc727886bdb46b57cb85812bf70
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-08-27 14:41:18 +0200

    Update App_personal_notes.py

--- App_personal_notes.py
+++ App_personal_notes.py
@@ -132,7 +132,8 @@ elif selected == "Microsoft Fabric" :
             selected_notebook = st.radio(
                 "",
                 ["Introduction", "Create tables", "Time travel", "Print and display", "Update a table", "Add columns", "Use SQL", "Data Wrangler", "Sempy (Semantic link)", "Other Spark functions & utilities"],
-                label_visibility = "collapsed"
+                label_visibility = "collapsed",
+                horizontal = True
             )
             #option_menu(
             #    None, 
