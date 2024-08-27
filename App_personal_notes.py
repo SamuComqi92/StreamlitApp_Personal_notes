ffa8035d51709b136c18a435e8a6755f2196cc4e
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-08-27 14:13:44 +0200

    Update App_personal_notes.py

--- App_personal_notes.py
+++ App_personal_notes.py
@@ -62,6 +62,10 @@ with st.sidebar:
         }
     )
 
+    myargs = [ "Made in ", footer.image_render('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4', width = px(25), height = px(25)), 
+        " by ", footer.link_render("https://www.linkedin.com/in/samuele-campitiello-ph-d-913b90104/", "Samuele Campitiello") ]
+    footer.footer(*myargs)
+
 
 ########### Landing page ############
 if selected == "Landing page" :
