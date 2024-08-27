b96442e4692db5e8a379ecf5cb6d139091c23daf
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-08-27 11:54:21 +0200

    Update App_personal_notes.py

--- App_personal_notes.py
+++ App_personal_notes.py
@@ -41,11 +41,10 @@ st.markdown(
 
 # Section navigation style
 Nav_styles = {
-    "container": {"padding": "0!important", "background-color": "#dbdbdb"},
+    "container": {"padding": "2!important", "background-color": "#dbdbdb"},
     "nav-link": {"font-size": "16px", "text-align": "center", "margin": "3px", "--hover-color": "#c7c5c5"},
     "nav-link-selected": {"background-color": "#239583"},
 }
-
 
 # --------------------------------------------------------------------------------------------------------------------------------------------
 # Menu laterale
