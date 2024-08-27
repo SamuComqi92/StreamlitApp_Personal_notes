b56604ea33f2604247a877427d6d14461ae1abd6
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-08-27 09:53:50 +0200

    Update App_personal_notes.py

--- App_personal_notes.py
+++ App_personal_notes.py
@@ -13,7 +13,7 @@ with st.sidebar:
         icons = ['house', 'gear'], 
         menu_icon = "cast", 
         default_index = 0,
-        styles = { "container": {"padding": "0!important", "border-radius": "10px"} }
+        styles = { "container": {"padding": "0!important", "border-radius": "5px"} }
     )
     
 if selected == "Qlik":
