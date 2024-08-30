541b53d1dad83ebb8c0af0c4f73482e3c91dc878
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-08-30 09:16:19 +0200

    Update App_personal_notes.py

--- App_personal_notes.py
+++ App_personal_notes.py
@@ -109,7 +109,8 @@ DP203_2_ADLS_BigData = load_text("Text/DP_203/DP203_2_ADLS_BigData.txt")
 with st.sidebar:
     main_selected = st.selectbox(
         "Choose a topic",
-        Main_topics
+        index = None,
+        placeholder = "Choose a topic..."
     )
     #main_selected = option_menu(
     #    "Topics", 
