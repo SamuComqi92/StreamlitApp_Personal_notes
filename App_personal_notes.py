62746f95dbf4233901691e95d46602aa80068abe
SamuComqi92 <90097187+SamuComqi92@users.noreply.github.com>
2024-09-03 09:22:04 +0200

    Update App_personal_notes.py

--- App_personal_notes.py
+++ App_personal_notes.py
@@ -519,8 +519,8 @@ elif main_selected == "Python notes" :
         st.write("Navigate in one of these pages:")
         selected_python = option_menu(
             None,
-            ["Data types", "print & input", "Operators", "Formatting", "Sequences", "Sets & Dictionaries", "Accessing elements", "Data type conversion"], 
-            icons = ["file-earmark", "file-earmark", "file-earmark", "file-earmark", "file-earmark", "file-earmark", "file-earmark", "file-earmark"], 
+            ["Data types", "Operators", "Formatting", "Sequences", "Sets & Dictionaries", "Accessing elements", "Data type conversion"], 
+            icons = ["file-earmark", "file-earmark", "file-earmark", "file-earmark", "file-earmark", "file-earmark", "file-earmark"], 
             default_index = 0,
             orientation = "horizontal",
             styles = {
